71122c912e580fc216af6698a8cf06e721a45f9d
yangchangpei <646742615@qq.com>
2017-03-01 18:21:18 +0800

    MessageController Bug Fixed

--- web/src/main/java/org/qfox/wectrl/web/MessageController.java
+++ web/src/main/java/org/qfox/wectrl/web/MessageController.java
@@ -3,16 +3,19 @@ package org.qfox.wectrl.web;
 import org.qfox.jestful.core.annotation.GET;
 import org.qfox.jestful.core.annotation.Jestful;
 import org.qfox.jestful.core.annotation.POST;
+import org.qfox.jestful.core.annotation.Query;
+import org.springframework.stereotype.Controller;
 
 /**
  * Created by yangchangpei on 17/3/1.
  */
 @Jestful("/message")
+@Controller
 public class MessageController {
 
     @GET("/")
-    public String validate() {
-        return "@:";
+    public String validate(@Query("echostr") String echostr) {
+        return "@:" + echostr;
     }
 
     @POST("/")
